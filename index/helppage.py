90d53d7848400b59cd153c9bac0b44d8382ea70d
syakesaba <syakesaba@gmail.com>
2014-02-11 19:41:19 +0900

    added sqli help

--- index/helppage.py
+++ index/helppage.py
@@ -13,7 +13,20 @@ class Help(webapp2.RequestHandler):
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
-助けが欲しいか？</br>
+<pre>
+<h4>SQLの仕様</h4>
+MariaでもPostgreでもMyでもありません。
+sqlite3というPythonの簡易データベースツールです。
+COPY文など使用できないstatementが多数存在します。
+一つ、大きな特徴として、「select * from table;select * from table;」
+のような、select文においてmultiple statementな
+SQLクエリを発行することができません。
+詳しくは<a href="http://docs.python.jp/2.6/library/sqlite3.html">python-sqlite3の公式Doc</a>をご覧ください。
+<h4>XSSの仕様</h4>
+未実装。
+phantomjsガー
+外部との通信ガー
+</pre>
 </body>
 </html>
 """)
